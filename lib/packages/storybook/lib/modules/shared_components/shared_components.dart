74cdb4998d1fb5283bfa54f752283068c573dc2d
francisco_diniz <francisco.neto@faci.ly>
2021-10-26 22:40:46 -0300

    chore: delete package storybook

--- lib/packages/storybook/lib/modules/shared_components/shared_components.dart
+++
@@ -1,9 +0,0 @@
-import 'package:finance/components/entry_manager/views/widgets/graph.dart';
-import 'package:finance/packages/storybook/lib/modules/shared_components/graphics.dart';
-import 'package:storybook_flutter/storybook_flutter.dart';
-
-class SharedComponents {
-  static Iterable<Story> get stories => [
-   ...Graphics.stories,
-  ]; 
-}
