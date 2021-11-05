19583cf66f88a7e84141db13e279ea557ab8f2bc
Henrique Lopes Lima <henriquelopeslima.hll@gmail.com>
2021-11-04 21:01:12 -0300

    chore: create bottom navigation bar

---
+++ packages/domain/lib/entities/custom_bottom_app_contract.dart
@@ -0,0 +1,6 @@
+import 'package:domain/core/contract_name.dart';
+import 'package:domain/entities/contract.dart';
+
+class CustomBottomAppContract extends Contract {
+  CustomBottomAppContract() : super(ContractName.customBottomApp);
+}
--- packages/storybook/lib/modules/shared_components/shared_components.dart
+++ packages/storybook/lib/modules/shared_components/shared_components.dart
@@ -1,4 +1,5 @@
 import 'package:storybook/modules/shared_components/custom_app_bar_story.dart';
+import 'package:storybook/modules/shared_components/custom_bottom_navigation_bar_story.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
 
 import 'graphics_story.dart';
@@ -7,5 +8,6 @@ class SharedComponents {
   static Iterable<Story> get stories => [
    ...GraphicsStory.stories,
    ...CustomAppBarStory.stories,
+   ...CustomBottomNavigationBarStory.stories,
   ]; 
 }
